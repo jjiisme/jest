b5e67107220490d3c965daf64e206179f1014ad4
jjiisme <jiangji512@gmail.com>
2025-04-12 02:55:53 +0800

    jji--password-check

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,20 @@
 import type { Config } from '@jest/types';
 
+const baseDir = '<rootDir>/src/app/pass_checker';
+const baseTestDir = '<rootDir>/src/test/pass_checker';
+
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // 控制是否在终端显示每个测试文件中 每个测试用例的运行结果
   verbose: true,
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/app/**/*.ts'],
+  collectCoverageFrom: [
+    // '<rootDir>/src/app/**/*.ts'
+    `${baseDir}/**/*.ts`,
+  ],
+  // 用于指定 哪些文件 会被 Jest 当作测试文件来运行
+  testMatch: [`${baseTestDir}/**/*.ts`],
 };
 
 export default config;
